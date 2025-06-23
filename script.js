8526115102ec439920dbfef7fea89835dfbf13fd
Sakshi Priya <priyasakshi8989@gmail.com>
2025-06-23 19:17:05 +0530

    added some codes in script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const question = [
+    {
+        question: "What is the capital of India?",
+        options: ["Mumbai", "Delhi", "Chennai", "Kolkata"],
+        answer: "Delhi"
+    },
+
+    {
+        question: "Which planet is known as the Red Planet?",
+        option: ["Earth", "Venus", "Mars", "Jupiter"],
+        answer: "Mars"
+    },
+
+    {
+        question: "What is 5 + 3?",
+        option: ["5", "8", "10", "7"],
+        answer: "8"
+    },
+
+    {
+        question: "Which language is used to style web pages?",
+        option: ["HTML", "CSS", "Python", "C++"],
+        answer: "CSS"
+    },
+
+    {
+        question: "Which is the smallest continent?",
+        option: ["Asia", "Africa", "Australia", "Europe"],
+        answer: "Australia"
+    }
+];
